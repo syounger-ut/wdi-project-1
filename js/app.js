2bdd255fcb31e1028399e45370874ccd596e9a80
samyounger <sam_younger@hotmail.com>
2016-08-20 19:58:55 +0100

    tried to get the ARC of the cat working, not succeeded yet.

--- js/app.js
+++ js/app.js
@@ -6,21 +6,64 @@ $(document).ready(function() {
   }, {
     duration: 5000,
     step: function(currentStep){
-      var politiciationPosition = $politician.offset();
+      var polaticianPosition = $politician.offset();
       var catPosition           = $cat.offset();
-
-      if (catPosition.left >= (politiciationPosition.left - 50) && 
-        catPosition.left <= (politiciationPosition.left + 50)) {
-        $('#politician').stop();
-        $('#politician').effect('explode');
-      }
+      collision(polaticianPosition, catPosition, politician, cat);
     }
   });
+//=================================================================
+// increase left & right positions incrementally in a loop as element moves.
+// this reverses when half-way is reached.
+// the thing which is changed is the final destination.
+
+// use setInterval loop to increase the numbers.
+  var timeout = setTimeout(arc, 1000);
+  function arc() {
+    // do something
+
+    timeout = setTimeout(arc, 1000)
+  }
+  function abortTimer() {
+    clearTimeout(timeout); //to be called to stop the timer
+  }
+
+  //=================================================================
+
+
   $( document).click(catMove);
 });
+
+function collision(polaticianPosition, catPosition, politician, cat) {
+  if (catPosition.left >= (polaticianPosition.left - 50) &&
+      catPosition.left <= (polaticianPosition.left + 50) &&
+      catPosition.top >= (polaticianPosition.top - 50) &&
+      catPosition.top <= (polaticianPosition.top + 50)) {
+    $(politician).stop();
+    $(politician).effect('explode');
+  }
+}
 
 function catMove() {
   var x = event.pageX-25;
   var y = event.pageY-25;
-  $("#cat").css( {position:"absolute", top:y, left:x});
+  $('#cat').animate( {top:+y, left:x},{duration: 1000});
+  halfWay(x,y);
+}
+
+function halfWay(x,y) {
+  var $center = $(window).width()/2;
+  var halfWayX = ((x-$center)/2 < 0) ? (x-$center)/2 *-1: (x-$center)/2;
+  var halfWayY = y/2;
+  console.log("X HalfWay = " + halfWayX + " Y HalfWay " + halfWayY)
+  if(x === halfWay)
+  $('#cat').css("background", "black");
+}
+
+function direction(x) {
+  var $center = $(window).width()/2;
+  if(x<$center) {
+    // do something
+  } else {
+    // do something else
+  }
 }
