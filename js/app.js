5f8de8b9fcd9783a96bc12c0b3b042bdd04ff5d0
samyounger <sam_younger@hotmail.com>
2016-08-21 17:26:33 +0100

    added random number generator for starting position on page

--- js/app.js
+++ js/app.js
@@ -1,46 +1,59 @@
+var playerScore = 0;
+var politicianNo = 0;
+
 $(document).ready(function() {
+  $( document).click(catMove);
+  startPosition();
+});
+
+function startPosition() {
+  var generator = Math.random() * (250 - 0) + 0;
+  setBoard(generator);
+}
+
+function setBoard(startPosition) {
+  $('.gameContainer').append("<div id='politician'></div>");
+  $('.gameContainer').append("<div id='cat'></div>");
+  $('#politician').offset({left: startPosition, top: startPosition})
+  movePolitician();
+}
+
+function movePolitician() {
+  politicianNo++;
   var $politician = $('#politician');
   var $cat        = $('#cat');
   $politician.animate({
-    left:'+=65.3%'
+    left:'+=65.3%', top: "+=50%"
   }, {
     duration: 5000,
-    step: function(currentStep){
-      var polaticianPosition = $politician.offset();
+    step: function(now, fx){
+      var test = /*fx.elem.id + " " + fx.prop + ": " + */fx;
+      $('#politician').css('left', fx + 100);
+      var politicianPosition    = $politician.offset();
       var catPosition           = $cat.offset();
-      collision(polaticianPosition, catPosition, politician, cat);
+      collision(politicianPosition, catPosition, politician, cat);
+    },
+    complete: function() {
+          $('#politician').remove();
+          $('#cat').remove();
+          startPosition();
     }
   });
-//=================================================================
-// increase left & right positions incrementally in a loop as element moves.
-// this reverses when half-way is reached.
-// the thing which is changed is the final destination.
+};
 
-// use setInterval loop to increase the numbers.
-  var timeout = setTimeout(arc, 1000);
-  function arc() {
-    // do something
-
-    timeout = setTimeout(arc, 1000)
-  }
-  function abortTimer() {
-    clearTimeout(timeout); //to be called to stop the timer
-  }
-
-  //=================================================================
-
-
-  $( document).click(catMove);
-});
-
-function collision(polaticianPosition, catPosition, politician, cat) {
-  if (catPosition.left >= (polaticianPosition.left - 50) &&
-      catPosition.left <= (polaticianPosition.left + 50) &&
-      catPosition.top >= (polaticianPosition.top - 50) &&
-      catPosition.top <= (polaticianPosition.top + 50)) {
-    $(politician).stop();
-    $(politician).effect('explode');
-  }
+function collision(politicianPosition, catPosition, politician, cat) {
+  if  (catPosition.left >= (politicianPosition.left - 50) &&
+      catPosition.left  <= (politicianPosition.left + 50) &&
+      catPosition.top   >= (politicianPosition.top - 50) &&
+      catPosition.top   <= (politicianPosition.top + 50)) {
+        $(politician).stop();
+        $(politician).effect('explode');
+        playerScore+= 0.5;
+        $('#playerScore').html(playerScore);
+        $('#politician').remove();
+        $('#cat').remove();
+        startPosition();
+      }
 }
 
 function catMove() {
@@ -56,7 +69,7 @@ function halfWay(x,y) {
   var halfWayY = y/2;
   console.log("X HalfWay = " + halfWayX + " Y HalfWay " + halfWayY)
   if(x === halfWay)
-  $('#cat').css("background", "black");
+    $('#cat').css("background", "black");
 }
 
 function direction(x) {
